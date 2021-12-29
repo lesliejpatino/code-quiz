d1b30c4af2d156b5301748dddab05e6d3a691fd3
Leslie Patino <lesliejpatino@gmail.com>
2021-12-28 17:56:20 -0800

    added some initial styling to the page and entered questions and answers for the quiz

---
+++ Assets/javascript/quiz.js
@@ -0,0 +1,18 @@
+/*
+REGULAR FLOW OF PAGE:
+Step 1: if user makes a selection 
+
+/*
+TIMER:
+Step 1: Create a timer for 1 min, 10 secs that will run throughout entirety of webpage upon click of the start button
+
+Step 2: IF user clicks on X, then highlight answer and make it appear bigger
+        
+        ELSE, change answer to "INCORRECT. -3 seconds" and remove 3 seconds from the timer
+        Repeat this for each question
+
+Step 3: If timer runs out, display score and have them input their initials & save
+        If they finish before the timer runs out, display score and have them input their initials & save
+*/
+
+
