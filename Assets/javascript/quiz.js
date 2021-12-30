a97e00e7aae47a8091c7dc9748fdd5b4444c57f4
Leslie Patino <lesliejpatino@gmail.com>
2021-12-30 11:39:48 -0800

    added a page that will display users final score

--- Assets/javascript/quiz.js
+++ Assets/javascript/quiz.js
@@ -1,5 +1,3 @@
-
-
 function timer(){
     var sec = 70;
     var timer = setInterval(function(){
@@ -12,21 +10,3 @@ function timer(){
 }
 
 timer();
-
-document.getElementById("correct").addEventListener("click", correct);
-
-function correct() {
-    document.getElementById("correct").innerHTML = "YES!";
-    document.getElementById("correct").style.color = "white";
-    document.getElementById("correct").style.backgroundColor = "green";
-    document.getElementById("correct").style.fontSize = "28px";    
-};
-
-document.querySelector(".incorrect").addEventListener("click", incorrect);
-
-function incorrect() {
-    document.querySelector(".incorrect").innerHTML = "WRONG";
-    document.querySelector(".incorrect").style.color = "white";
-    document.querySelector(".incorrect").style.backgroundColor = "red";
-    document.querySelector(".incorrect").style.fontSize = "28px";
-};
