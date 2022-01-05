eee66858dc9a09891721f3c930b0888cdcb2ab1d
Leslie Patino <lesliejpatino@gmail.com>
2022-01-04 17:49:43 -0800

    cleaned up javascript code in order for the questions to move in the correct order if the incorrect answer was selected

--- Assets/javascript/quiz.js
+++ Assets/javascript/quiz.js
@@ -1,4 +1,4 @@
-// created a 70 second timer that will remain on the page for the duration of the quiz
+// created a 60 second timer that will remain on the page for the duration of the quiz
 function timer(){
     window.sec = 60;
     var timer = setInterval(function(){
@@ -16,12 +16,8 @@ function timer(){
 
 timer();
 
-
-
-
 // Created a variable for the Start Quiz Button 
 var generateBtn = document.querySelector("#start-quiz");
-
 
 var correctButtons = document.querySelectorAll(".correct")
 //Created a for loop that will move on to the next question when a user selects the correct answer
@@ -31,7 +27,7 @@ for (let i = 0; i < correctButtons.length; i++) {
     function correct(e) {
         e.target.parentElement.parentElement.parentElement.style="display:none";
         var next = i+1
-        document.getElementById("q"+next).style="display:block"
+        document.getElementById("q"+next).style="display:block";
     };
 }
 
@@ -43,73 +39,7 @@ for (let i = 0; i < incorrectButtons.length; i++) {
     function incorrect(e) {   
         window.sec-=5;
         e.target.parentElement.parentElement.parentElement.style="display:none";
-        if (i==1) {
-            i-=1
-        }
-        if (i == 2) {
-            i-=2
-        }
-
-        if (i == 3) {
-            i-=2
-        }
-
-        if (i == 4) {
-            i-=3
-        }
-        if (i == 5) {
-            i-=4
-        }
-
-        if (i == 6) {
-            i-=4
-        }
-
-        if (i == 7) {
-            i-=5
-        }
-
-        if (i == 8) {
-            i-=6
-        }
-
-        if (i == 9) {
-            i-=6
-        }
-
-        if (i == 10) {
-            i-=7
-        }
-
-        if (i == 11) {
-            i-=8
-        }
-
-        if (i == 12) {
-            i-=8
-        }
-
-        if (i == 13) {
-            i-=9
-        }
-
-        if (i == 14) {
-            i-=10
-        }
-
-        if (i == 15) {
-            i-=10
-        }
-
-        if (i == 16) {
-            i-=11
-        }
-
-        if (i == 17) {
-            i-=12
-        }
-
-        var next = i+1
+        var next = Math.floor(i/3)+1
         document.getElementById("q"+next).style="display:block"
     };
 }
